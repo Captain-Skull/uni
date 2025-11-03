9e7ea48bd15cd34ccf5dbd110f4fd2ad2cf863e0
George Mkrtchyan <84709250+NegodayCaptainSkull@users.noreply.github.com>
2025-11-03 15:18:52 +0300

    Add files via upload

---
+++ 1term/inf/11SequentialContainers/11SequentialContainers/11SequentialContainers.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <random>
+#include <array>
+#include <list>
+#include<string>
+#include <windows.h>
+#include <forward_list>
+
+using namespace std;
+
+int random_int(int min, int max) {
+    static random_device rd;
+    static mt19937 gen(rd());
+    uniform_int_distribution<int> dist(min, max);
+    return dist(gen);
+}
+
+void arrayPrinter(array<int, 10> arr) {
+    cout << "Array: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+void listPrinter(list<string> arr) {
+    cout << "Array: ";
+    for (string s : arr) {
+        cout << s << " ";
+    }
+    cout << endl;
+}
+
+void listPrinter(forward_list<int> arr) {
+    cout << "Array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+void listPrinter(list<int> arr) {
+    cout << "Array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+void task1() {
+    array<int, 10> numsArr;
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        numsArr[i] = random_int(1, 10);
+        sum += numsArr[i];
+    }
+
+    arrayPrinter(numsArr);
+    double middle = sum / static_cast<double>(10);
+    cout << middle;
+}
+
+void task3() {
+    list<string> students = { "Иванов", "Петров", "Сидоров", "Алексеев", "Борисов" };
+    students.remove_if([](string& student) {return student[0] == 'А';  });
+    students.sort([](const string& s1, const string& s2) { return s1 < s2; });
+
+    listPrinter(students);
+}
+
+void task4() {
+    forward_list<int> fwlist;
+    auto it = fwlist.before_begin();
+    it = fwlist.insert_after(it, 5);
+    it = fwlist.insert_after(it, 4);
+    it = fwlist.insert_after(it, 3);
+    it = fwlist.insert_after(it, 2);
+    it = fwlist.insert_after(it, 1);
+
+    fwlist.reverse();
+
+    listPrinter(fwlist);
+}
+
+void task5() {
+    list<int> a = { 1, 3, 5, 7 };
+    list<int> b = { 2, 3, 6, 7, 8 };
+    a.merge(b);
+    a.unique();
+    a.remove_if([](int& x) { return x > 6;  });
+    listPrinter(a);
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+	task4();
+}
---
+++ 1term/inf/12Files/12Files/12Files.cpp
@@ -0,0 +1,95 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void task1() {
+	ifstream input("input.txt");
+	ofstream output("output.txt");
+
+	string phrase = "useragreement";
+	string text;
+
+	getline(input, text);
+
+	for (char c : text) {
+		char low_c = tolower(c);
+
+		if (phrase.find(c) == string::npos) {
+			output << c;
+		}
+	}
+
+	input.close();
+	output.close();
+}
+
+void task2() {
+	ifstream input1("input1.txt");
+	ifstream input2("input2.txt");
+
+	string x;
+	string y;
+	getline(input1, x);
+	getline(input2, y);
+
+	if (x.find(y) != string::npos) {
+		cout << "yes";
+	}
+	else {
+		cout << "no";
+	}
+}
+
+string convertToBin(string numberStr) {
+	int num = stoi(numberStr);
+	string binaryNum = "";
+	while (num > 0) {
+		binaryNum += to_string(num % 2);
+		num = num / 2;
+	}
+
+	reverse(binaryNum.begin(), binaryNum.end());
+	cout << binaryNum << endl;
+
+	return binaryNum;
+}
+
+void task3() {
+	ifstream inputNum("inputNum.txt");
+	ofstream outputNum("outputNum.txt");
+
+	string nums = "0123456789";
+	string text;
+	getline(inputNum, text);
+
+	bool flag = false;
+	string num = "";
+
+	for (char c : text) {
+		if (nums.find(c) != string::npos) {
+			num += c;
+			flag = true;
+		}
+		else if (flag) {
+			string binaryNum = convertToBin(num);
+			outputNum << binaryNum;
+			outputNum << c;
+			flag = false;
+			num = "";
+		}
+		else {
+			outputNum << c;
+		}
+	}
+
+	if (num != "") {
+		string binaryNum = convertToBin(num);
+		outputNum << binaryNum;
+	}
+}
+
+int main() {
+	task3();
+}
