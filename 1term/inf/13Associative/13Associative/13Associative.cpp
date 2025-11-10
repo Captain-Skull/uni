d53b2b11af95220ac66b79dae1f18a2945229100
George Mkrtchyan <84709250+NegodayCaptainSkull@users.noreply.github.com>
2025-11-10 15:15:36 +0300

    Add files via upload

---
+++ 1term/inf/13Associative/13Associative/13Associative.cpp
@@ -0,0 +1,75 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+void mapPrinter(map<char, char> arr) {
+    for (const auto& pair : arr) {
+        cout << pair.first << " ";
+    }
+    cout << endl;
+}
+
+void task1() {
+    vector<string> words = { "apple", "apache", "ear", "peach" };
+    map<char, char> letters;
+
+    for (char c : words[0]) {
+        letters[c] = c;
+    }
+
+    for (int i = 1; i < words.size(); i++) {
+        map<char, char> newLetters;
+        for (const auto& pair : letters) {
+            char c = pair.first;
+            if (words[i].find(c) != string::npos) {
+                newLetters[c] = c;
+            }
+        }
+        letters = newLetters;
+    }
+
+    mapPrinter(letters);
+}
+
+void task2() {
+	int n;
+	cin >> n;
+	unordered_map<string, vector<int>> book;
+
+	for (int i = 0; i < n; i++) {
+		string word;
+		int page;
+		cin >> word >> page;
+		book[word].push_back(page);
+	}
+
+	map<int, map<string, string>> outputBook;
+
+    for (const auto& pair : book) {
+        string word = pair.first;
+        vector<int> pages = pair.second;
+
+        if (pages.size() != 1) {
+            continue;
+        }
+
+        outputBook[pages[0]][word] = word;
+    }
+
+    for (const auto& pair : outputBook) {
+        int page = pair.first;
+        map<string, string> wordMap = pair.second;
+        auto& firstWordPair = *wordMap.begin();
+        string word = firstWordPair.first;
+
+        cout << page << " " << word;
+    }
+}
+
+int main() {
+	task2();
+}
---
+++ 1term/inf/14Algorithms/14Algorithms/14Algorithms.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+void vectorPrinter(vector<T> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+template <typename T>
+vector<T> Duplicate(std::vector<T>& v) {
+    vector<T> newV;
+
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        newV.push_back(*it);
+    }
+
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        newV.push_back(*it);
+    }
+
+    return newV;
+}
+
+int main()
+{
+    vector<int> firstV = { 1, 2, 4 };
+    firstV = Duplicate(firstV);
+
+    vectorPrinter(firstV);
+}
